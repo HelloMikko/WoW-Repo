c517ddb591e3956ac4f0a5478ee04352b1c08475
Michael <icuemike@outlook.com>
2022-08-13 02:19:53 -0400

    Create Nameplate Style Filters.js

---
+++ ElvUI-Profiles/Warlock/NaowhUI/Nameplate Style Filters.js
@@ -0,0 +1,264 @@
+// Just like the "Profile.js" codes, follow the same steps and import this as well
+
+{
+    ["nameplates"] = {
+        ["filters"] = {
+            ["ElvUI_Explosives"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["healthColor"] = {
+                            ["b"] = 1,
+                            ["g"] = 0,
+                            ["r"] = 0.49019607843137,
+                        },
+                    },
+                    ["scale"] = 1,
+                    ["usePortrait"] = false,
+                },
+                ["triggers"] = {
+                    ["priority"] = 1,
+                },
+            },
+            ["ElvUI_NonTarget"] = {
+                ["actions"] = {
+                    ["alpha"] = -1,
+                },
+            },
+            ["ElvUI_Target"] = {
+                ["actions"] = {
+                    ["scale"] = 1,
+                },
+            },
+            ["Luckyone_DOS"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["164857"] = true,
+                        ["167965"] = true,
+                        ["168934"] = true,
+                        ["168942"] = true,
+                        ["169905"] = true,
+                        ["170572"] = true,
+                        ["171341"] = true,
+                        ["171343"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_Encrypted"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["b"] = 0.42745098039216,
+                            ["g"] = 0.6078431372549,
+                            ["r"] = 0.77647058823529,
+                        },
+                    },
+                    ["tags"] = {
+                        ["name"] = "[name]",
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["184908"] = true,
+                        ["184910"] = true,
+                        ["184911"] = true,
+                        ["185680"] = true,
+                        ["185683"] = true,
+                        ["185685"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_HOA"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["164562"] = true,
+                        ["165529"] = true,
+                        ["167612"] = true,
+                        ["167892"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_MISTS"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["164804"] = true,
+                        ["164921"] = true,
+                        ["166299"] = true,
+                        ["167111"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_NW"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["163618"] = true,
+                        ["165824"] = true,
+                        ["165872"] = true,
+                        ["166302"] = true,
+                        ["173016"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_PF"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["163882"] = true,
+                        ["163894"] = true,
+                        ["164737"] = true,
+                        ["168572"] = true,
+                        ["168627"] = true,
+                        ["169861"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_SD"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["162038"] = true,
+                        ["162040"] = true,
+                        ["162057"] = true,
+                        ["171376"] = true,
+                        ["171799"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_SOA"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["163459"] = true,
+                        ["163520"] = true,
+                        ["168318"] = true,
+                        ["168681"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_TOP"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["160495"] = true,
+                        ["164451"] = true,
+                        ["164506"] = true,
+                        ["170850"] = true,
+                        ["174210"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["Luckyone_TZ"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.75686274509804,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["178141"] = true,
+                        ["179733"] = true,
+                        ["180431"] = true,
+                        ["180433"] = true,
+                    },
+                    ["priority"] = 2,
+                },
+            },
+            ["MerathilisUI_Neutral"] = {
+                ["actions"] = {
+                    ["nameOnly"] = true,
+                },
+                ["triggers"] = {
+                    ["notTarget"] = true,
+                    ["outOfCombat"] = true,
+                    ["outOfVehicle"] = true,
+                    ["reactionType"] = {
+                        ["enable"] = true,
+                        ["neutral"] = true,
+                        ["reputation"] = true,
+                    },
+                },
+            },
+        },
+    },
+}::styleFilters
